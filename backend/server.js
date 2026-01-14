07fa8bce2fab21ec6e9094409a60ce467585aef4
Arjit Dhuria <arjitdhuria01@gmail.com>
2026-01-15 00:21:00 +0530

    Prevent backend crash on MongoDB connection failure

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 const app = require("./src/app");
 const connectDB = require("./src/config/db");
 
-connectDB();
 
 const PORT = process.env.PORT || 5000;
+
+connectDB();
+
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
